ebfc573ddbb7593e5b9a3b2ef4ea5046ed24dca9
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-19 02:56:36 -0300

    modulo 7

--- treinaweb/http/app.js
+++ treinaweb/http/app.js
@@ -10,14 +10,14 @@ const server = http.createServer((req, res) => {
     res.writeHead('200', { 'content-type': 'text/html' });
     var url = req.url;
     if (url === '/sobre') {
-        res.write('<strong><h1>Treianmento Sobre</h1></strong>');
+        res.write('<strong><h1>Treinamento Sobre</h1></strong>');
         res.write('<em><p>validando servido Node.js</p></em>');
         res.end();
     } else if (url === '/contato') {
-        res.write('<h1>TreinWeb Contato</h1>')
+        res.write('<h1>TreinaWeb Contato</h1>')
         res.end();
     } else {
-        res.write('<h1>TreinaWeb</h1><br><a href="/sobre">Sobre</a><br> href="/contato">Contato</a> ');
+        res.write('<h1>TreinaWeb</h1><br><a href="/sobre">Sobre</a><br><a href="/contato">Contato</a> ');
         res.end();
     }
 
