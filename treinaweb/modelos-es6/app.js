0902e33937aee3022cf9726e4269e3a25c6a4ab8
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-19 01:30:42 -0300

    ajuste aula 4

--- treinaweb/modelos-es6/app.js
+++ treinaweb/modelos-es6/app.js
@@ -1,5 +1,7 @@
 import * as ModuloPessoa from './pessoa.js';
+//import abc from './pessoa.js';
 
 console.log(ModuloPessoa.pessoa.nome);
 ModuloPessoa.pessoa.andar();
 console.log(ModuloPessoa.somar());
+//console.log(abc());
