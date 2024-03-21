0bd1b9ed7db10881b0691b2c274f56238e991c8a
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-20 23:20:27 -0300

    ajuste classe

--- treinaweb/PDFGenerator/app.js
+++ treinaweb/PDFGenerator/app.js
@@ -1,16 +1,14 @@
-import * as http from 'http'
-import * as fs from 'fs'
-import * as pdf from 'html-pdf'
+import * as http from 'http';
+import * as fs from 'fs';
+import * as pdf from 'html-pdf';
 
 const server = http.createServer((req, res) => {
-    var html = fs.readFileSync('index.html', 'utf8')
-
+    var html = fs.readFileSync('index.html', 'utf8');
     const currentDate = new Date().toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
         year: 'numeric',
     });
-
 
     html = html.replace('{{data_atual}}', currentDate);
 
@@ -19,18 +17,17 @@ const server = http.createServer((req, res) => {
         orientation: 'portrait'
     };
 
-    pdf.create(html, options).toBuffer((err, Buffer) => {
+    pdf.create(html, options).toBuffer((err, buffer) => {
         if (err) {
             res.statusCode = 500;
             res.setHeader('Content-Type', 'application/json');
             return res.end(JSON.stringify(err));
         }
         res.setHeader('Content-Type', 'application/pdf');
-        res.end(Buffer);
+        res.end(buffer);
     });
 });
 
-server.listen('3000', () => {
-    console.log('Servidor iniciado na prota 3000!');
-    
+server.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000!');
 });
