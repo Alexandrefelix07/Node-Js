5f2cf07c02b87d6ccb15f89193ac6537224c3a6a
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-20 21:55:05 -0300

    ultimo events

--- treinaweb/events/app.js
+++ treinaweb/events/app.js
@@ -1,8 +1,23 @@
 import EventEmitter from 'events'
-var emitter = new EventEmitter();
+// var emitter = new EventEmitter();
 
-emitter.on('meu_evento', (num) => {
-    console.log('meu evento foi acionado !' + num)
-});
+// emitter.on('meu_evento', (num) => {
+//     console.log('meu evento foi acionado !' + num)
+// });
 
-emitter.emit('meu_evento', 123456);
+// emitter.emit('meu_evento', 123456);
+class Cao extends EventEmitter {
+    latir() {
+        console.log('Latindo....');
+    }
+}
+
+var rex = new Cao();
+
+rex.on('pessoa_na_porta', rex.latir);
+//rex.once('pessoa_na_porta', rex.latir);
+rex.emit('pessoa_na_porta');
+rex.emit('pessoa_na_porta');
+rex.removeListener('pessoa_na_porta', rex.latir);
+rex.emit('pessoa_na_porta');
+
