36ff89aa02f2563c640719b9d288d4bf0ce23eb7
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-21 23:08:38 -0300

    ts

---
+++ treinaweb/ts/app.ts
@@ -0,0 +1,10 @@
+// npm init > npm install typescript --D
+// npx tsc app.ts   para gerar o arquivo
+var a: string = "treinaweb";
+var b: number = 123;
+
+function exemplots(numero: number, texto: string) {
+  console.log(texto, numero);
+}
+
+exemplots(b, a);
