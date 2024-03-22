17169aba77dd9839db0215220e465e0e011af383
Alexandre Felix <alexandrefelix07@hotmail.com>
2024-03-21 22:30:37 -0300

    Promise aula 10

--- treinaweb/promises/app.js
+++ treinaweb/promises/app.js
@@ -1,13 +1,32 @@
-const p = new Promise((resolve, reject) => {
-    try {
-        resolve('Ok');
-    } catch (e) {
-        reject(e)
-    }
+import * as fs from 'fs'
+// const p = new Promise((resolve, reject) => {
+//     try {
+//         resolve('Ok');
+//     } catch (e) {
+//         reject(e)
+//     }
 
-});
-p.then(
-    (parametros) => console.log('sucesso', parametros)
+// });
+// p.then(
+//     (parametros) => console.log('sucesso', parametros)
+// ).catch(
+//     (erro) => console.log('error ', erro)
+// );
+
+function read(file) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data.toString());
+            }
+        });
+    });
+}
+
+read('meu_arquivo.txt').then(
+    (data) => console.log(data)
 ).catch(
-    (erro) => console.log('error ', erro)
+    (erro) => console.log('error tratado ', erro)
 );
